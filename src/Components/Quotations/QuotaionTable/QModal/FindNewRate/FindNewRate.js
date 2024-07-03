bbeea45af7c280f0b9acfef0da520b0cdb9cc6cd
Sugitha08 <142788570+Sugitha08@users.noreply.github.com>
2024-07-03 17:52:47 +0530

    serviceinclude

--- src/Components/Quotations/QuotaionTable/QModal/FindNewRate/FindNewRate.js
+++ src/Components/Quotations/QuotaionTable/QModal/FindNewRate/FindNewRate.js
@@ -11,12 +11,16 @@ import ExpandLessIcon from "@mui/icons-material/ExpandLess";
 import Search from "../../../../../assets/Search.png";
 import img from "../../../../../assets/Framethumbs.png";
 import QuoteRequest from "./QuoteRequest";
+import { Collapse } from "antd";
+import { DownOutlined, UpOutlined } from "@ant-design/icons";
 
 function FindNewRate() {
   const onChange = (e) => {
     console.log(`checked = ${e.target.checked}`);
   };
-
+  const onChangeCollapse = (key) => {
+    console.log(key);
+  };
   const Details = [
     {
       id: "0",
@@ -96,268 +100,237 @@ function FindNewRate() {
       TotalPrice: "$380",
     },
   ];
+  const item1 = [
+    {
+      key: "1",
+      label: "Origin",
+      children: (
+        <>
+          <p>
+            <Checkbox onChange={onChange}>Origin Charges</Checkbox>
+            <Tooltip
+              placement="topLeft"
+              title="Lorem ipsum dolor sit amet consectetur. Gravida id amet id maecenas tellus."
+            >
+              <span style={{ float: "right" }} role="button">
+                <img src={info} alt="more" />
+              </span>
+            </Tooltip>
+          </p>
+          <p>
+            <Checkbox onChange={onChange}>
+              Export Clearance
+              <span>
+                <img src={img} alt="icon" className="ms-2 mb-1" />
+              </span>
+            </Checkbox>
+            <Tooltip
+              placement="topLeft"
+              title="Lorem ipsum dolor sit amet consectetur. Gravida id amet id maecenas tellus."
+            >
+              <span style={{ float: "right" }} role="button">
+                <img src={info} alt="more" />
+              </span>
+            </Tooltip>
+          </p>
+          <p>
+            <Checkbox onChange={onChange}>Cargo Pickup</Checkbox>
+            <Tooltip
+              placement="topLeft"
+              title="Lorem ipsum dolor sit amet consectetur. Gravida id amet id maecenas tellus."
+            >
+              <span style={{ float: "right" }} role="button">
+                <img src={info} alt="more" />
+              </span>
+            </Tooltip>
+          </p>
+          <p>
+            <Checkbox onChange={onChange}>International Freight</Checkbox>
+            <Tooltip
+              placement="topLeft"
+              title="Lorem ipsum dolor sit amet consectetur. Gravida id amet id maecenas tellus."
+            >
+              <span style={{ float: "right" }} role="button">
+                <img src={info} alt="more" />
+              </span>
+            </Tooltip>
+          </p>
+        </>
+      ),
+    },
+  ];
+  const item2 = [
+    {
+      key: "1",
+      label: "Destination",
+      children: (
+        <>
+          {" "}
+          <p>
+            <Checkbox onChange={onChange}>Destination Charges</Checkbox>
+            <Tooltip
+              placement="topLeft"
+              title="Lorem ipsum dolor sit amet consectetur. Gravida id amet id maecenas tellus."
+            >
+              <span style={{ float: "right" }} role="button">
+                <img src={info} alt="more" />
+              </span>
+            </Tooltip>
+          </p>
+          <p>
+            <Checkbox onChange={onChange}>
+              Import Clearance
+              <span>
+                <img src={img} alt="icon" className="ms-2 mb-1" />
+              </span>
+            </Checkbox>
+            <Tooltip
+              placement="topLeft"
+              title="Lorem ipsum dolor sit amet consectetur. Gravida id amet id maecenas tellus."
+            >
+              <span style={{ float: "right" }} role="button">
+                <img src={info} alt="more" />
+              </span>
+            </Tooltip>
+          </p>
+          <p>
+            <Checkbox onChange={onChange}>Cargo Delivery</Checkbox>
+            <Tooltip
+              placement="topLeft"
+              title="Lorem ipsum dolor sit amet consectetur. Gravida id amet id maecenas tellus."
+            >
+              <span style={{ float: "right" }} role="button">
+                <img src={info} alt="more" />
+              </span>
+            </Tooltip>
+          </p>
+        </>
+      ),
+    },
+  ];
+  const item3 = [
+    {
+      key: "1",
+      label: "Value Added",
+      children: (
+        <>
+          <p>
+            <Checkbox onChange={onChange}>
+              Cargo Insurance
+              <span>
+                <img src={img} alt="icon" className="ms-2 mb-1" />
+              </span>
+            </Checkbox>
+            <Tooltip
+              placement="topLeft"
+              title="Lorem ipsum dolor sit amet consectetur. Gravida id amet id maecenas tellus."
+            >
+              <span style={{ float: "right" }} role="button">
+                <img src={info} alt="more" />
+              </span>
+            </Tooltip>
+          </p>
+        </>
+      ),
+    },
+  ];
+  const item4 = [
+    {
+      key: "1",
+      label: "Cargo Type",
+      children: (
+        <>
+          {" "}
+          <p>
+            <Checkbox onChange={onChange}>Stackable Cargo</Checkbox>
+            <Tooltip
+              placement="topLeft"
+              title="Lorem ipsum dolor sit amet consectetur. Gravida id amet id maecenas tellus."
+            >
+              <span style={{ float: "right" }} role="button">
+                <img src={info} alt="more" />
+              </span>
+            </Tooltip>
+          </p>
+          <p>
+            <Checkbox onChange={onChange}>Non Harzardous Cargo</Checkbox>
+            <Tooltip
+              placement="topLeft"
+              title="Lorem ipsum dolor sit amet consectetur. Gravida id amet id maecenas tellus."
+            >
+              <span style={{ float: "right" }} role="button">
+                <img src={info} alt="more" />
+              </span>
+            </Tooltip>
+          </p>
+        </>
+      ),
+    },
+  ];
+
+  const customExpandIcon = ({ isActive }) =>
+    isActive ? <UpOutlined /> : <DownOutlined />;
 
   return (
-    <div className="container-fluid p-0" style={{ backgroundColor: "#F8FAFC",Width:"100%",minWidth:"1255px"}} >
-      <div style={{ backgroundColor: "white" }}>
-        <div
-          style={{ maxWidth: "1255px", padding: "26px 0px 20px 0px" }}
-          className="mx-auto"
-        >
-          <div className="pb-3">
-            <Navbar />
-          </div>
-          <div className="location-card">
-            <Card
-              style={{
-                width: "100%",
-              }}
-              className="shadow"
-            >
-              <div className="row location-bar">
-                <div className="col d-flex cargo-loc me-4">
-                  <div className="align-self-center mx-2">
-                    <img src={Location} alt="loc" />
-                  </div>
-                  <div>
-                    <p className="p-label">Origin</p>
-                    {/* <img src="" alt="map" /> */}
-                    <p className="p-value">Nhava Sheva(INNSA)</p>
-                  </div>
-                  <div
-                    className="align-self-center"
-                    style={{ position: "absolute", marginLeft: "376px" }}
-                  >
-                    <img src={arrow} alt="arrow" />
-                  </div>
-                </div>
-                <div className="col d-flex cargo-loc">
-                  <div className="align-self-center mx-2">
-                    <img src={Location} alt="loc" />
-                  </div>
-                  <div>
-                    <p className="p-label">Destination</p>
-                    {/* <img src="" alt="map" /> */}
-                    <p className="p-value">Jebel Ali(AEJEA)</p>
-                  </div>
-                </div>
-                <div className="col d-flex">
-                  <div className="align-self-center mx-2">
-                    <img
-                      src="https://www.fslgo.com/_next/static/media/cargo.8d7c215b.svg"
-                      alt="loc"
-                    />
-                  </div>
-                  <div>
-                    <p className="p-label">Cargo</p>
-                    <p className="p-value">LCL, 2 Unit | Total : 100KG, 5CBM</p>
-                  </div>
-                  <div className="align-self-center ms-auto cargo-search">
-                    <img src={Search} alt="search p-2" />
-                  </div>
-                </div>
-              </div>
-            </Card>
-          </div>
+    <div
+      style={{
+        Width: "100%",
+        minWidth: "1255px",
+        padding: "20px",
+        backgroundColor: "#f3f5f7",
+        marginTop: "100px",
+      }}
+    >
+      <div className="quotationresult-div mx-auto">
+        <div className="quotationresult-leftdiv" style={{ flex: "0 0 272px" }}>
+          <Card title="Service Included">
+            <div className="Service-card">
+              <Collapse
+                defaultActiveKey={["1"]}
+                expandIconPosition="end"
+                expandIcon={customExpandIcon}
+                ghost={true}
+                onChange={onChangeCollapse}
+                className="width-full"
+                items={item1}
+                style={{ borderBottom: "1px solid #F3F5F7" }}
+              />
+              <Collapse
+                defaultActiveKey={["1"]}
+                expandIconPosition="end"
+                expandIcon={customExpandIcon}
+                ghost={true}
+                onChange={onChangeCollapse}
+                className="width-full"
+                items={item2}
+                style={{ borderBottom: "1px solid #F3F5F7" }}
+              />
+              <Collapse
+                defaultActiveKey={["1"]}
+                expandIconPosition="end"
+                expandIcon={customExpandIcon}
+                ghost={true}
+                onChange={onChangeCollapse}
+                className="width-full"
+                items={item3}
+                style={{ borderBottom: "1px solid #F3F5F7" }}
+              />
+              <Collapse
+                defaultActiveKey={["1"]}
+                expandIconPosition="end"
+                expandIcon={customExpandIcon}
+                ghost={true}
+                onChange={onChangeCollapse}
+                className="width-full"
+                items={item4}
+                style={{ borderBottom: "1px solid#F3F5F7" }}
+              />
+            </div>
+          </Card>
         </div>
-      </div>
-      <div>
-        <div style={{ maxWidth: "1255px" }} className="mx-auto">
-          <div className="row mt-4">
-            <div className="col-3 Service-Included mb-3">
-              <Card title="Service Included">
-                <div className="Service-card">
-                  <p className="service-title">
-                    Origin
-                    <span
-                      className="me-1"
-                      style={{
-                        float: "right",
-                        width: "16px",
-                        color: "#495a6e",
-                      }}
-                    >
-                      <ExpandLessIcon />
-                    </span>
-                  </p>
-                  <p>
-                    <Checkbox onChange={onChange}>Origin Charges</Checkbox>
-                    <Tooltip
-                      placement="topLeft"
-                      title="Lorem ipsum dolor sit amet consectetur. Gravida id amet id maecenas tellus."
-                    >
-                      <span style={{ float: "right" }} role="button">
-                        <img src={info} alt="more" />
-                      </span>
-                    </Tooltip>
-                  </p>
-                  <p>
-                    <Checkbox onChange={onChange}>
-                      Export Clearance
-                      <span>
-                        <img src={img} alt="icon" className="ms-2 mb-1" />
-                      </span>
-                    </Checkbox>
-                    <Tooltip
-                      placement="topLeft"
-                      title="Lorem ipsum dolor sit amet consectetur. Gravida id amet id maecenas tellus."
-                    >
-                      <span style={{ float: "right" }} role="button">
-                        <img src={info} alt="more" />
-                      </span>
-                    </Tooltip>
-                  </p>
-                  <p>
-                    <Checkbox onChange={onChange}>Cargo Pickup</Checkbox>
-                    <Tooltip
-                      placement="topLeft"
-                      title="Lorem ipsum dolor sit amet consectetur. Gravida id amet id maecenas tellus."
-                    >
-                      <span style={{ float: "right" }} role="button">
-                        <img src={info} alt="more" />
-                      </span>
-                    </Tooltip>
-                  </p>
-                  <p>
-                    <Checkbox onChange={onChange}>
-                      International Freight
-                    </Checkbox>
-                    <Tooltip
-                      placement="topLeft"
-                      title="Lorem ipsum dolor sit amet consectetur. Gravida id amet id maecenas tellus."
-                    >
-                      <span style={{ float: "right" }} role="button">
-                        <img src={info} alt="more" />
-                      </span>
-                    </Tooltip>
-                  </p>
-                </div>
-                <div className="Service-card">
-                  <p className="service-title">
-                    Destination
-                    <span
-                      className="me-1"
-                      style={{
-                        float: "right",
-                        width: "16px",
-                        color: "#495a6e",
-                      }}
-                    >
-                      <ExpandLessIcon />
-                    </span>
-                  </p>
-                  <p>
-                    <Checkbox onChange={onChange}>Destination Charges</Checkbox>
-                    <Tooltip
-                      placement="topLeft"
-                      title="Lorem ipsum dolor sit amet consectetur. Gravida id amet id maecenas tellus."
-                    >
-                      <span style={{ float: "right" }} role="button">
-                        <img src={info} alt="more" />
-                      </span>
-                    </Tooltip>
-                  </p>
-                  <p>
-                    <Checkbox onChange={onChange}>
-                      Import Clearance
-                      <span>
-                        <img src={img} alt="icon" className="ms-2 mb-1" />
-                      </span>
-                    </Checkbox>
-                    <Tooltip
-                      placement="topLeft"
-                      title="Lorem ipsum dolor sit amet consectetur. Gravida id amet id maecenas tellus."
-                    >
-                      <span style={{ float: "right" }} role="button">
-                        <img src={info} alt="more" />
-                      </span>
-                    </Tooltip>
-                  </p>
-                  <p>
-                    <Checkbox onChange={onChange}>Cargo Delivery</Checkbox>
-                    <Tooltip
-                      placement="topLeft"
-                      title="Lorem ipsum dolor sit amet consectetur. Gravida id amet id maecenas tellus."
-                    >
-                      <span style={{ float: "right" }} role="button">
-                        <img src={info} alt="more" />
-                      </span>
-                    </Tooltip>
-                  </p>
-                </div>
-                <div className="Service-card mt-2">
-                  <p className="service-title">
-                    Value Added
-                    <span
-                      className="me-1"
-                      style={{ float: "right", width: "16px" }}
-                    >
-                      <ExpandLessIcon />
-                    </span>
-                  </p>
-                  <p>
-                    <Checkbox onChange={onChange}>
-                      Cargo Insurance
-                      <span>
-                        <img src={img} alt="icon" className="ms-2 mb-1" />
-                      </span>
-                    </Checkbox>
-                    <Tooltip
-                      placement="topLeft"
-                      title="Lorem ipsum dolor sit amet consectetur. Gravida id amet id maecenas tellus."
-                    >
-                      <span style={{ float: "right" }} role="button">
-                        <img src={info} alt="more" />
-                      </span>
-                    </Tooltip>
-                  </p>
-                </div>
-                <div className="Service-card mt-2">
-                  <p className="service-title">
-                    Cargo Type
-                    <span
-                      className="me-1"
-                      style={{
-                        float: "right",
-                        width: "16px",
-                        color: "#495a6e",
-                      }}
-                    >
-                      <ExpandLessIcon />
-                    </span>
-                  </p>
-                  <p>
-                    <Checkbox onChange={onChange}>Stackable Cargo</Checkbox>
-                    <Tooltip
-                      placement="topLeft"
-                      title="Lorem ipsum dolor sit amet consectetur. Gravida id amet id maecenas tellus."
-                    >
-                      <span style={{ float: "right" }} role="button">
-                        <img src={info} alt="more" />
-                      </span>
-                    </Tooltip>
-                  </p>
-                  <p>
-                    <Checkbox onChange={onChange}>
-                      Non Harzardous Cargo
-                    </Checkbox>
-                    <Tooltip
-                      placement="topLeft"
-                      title="Lorem ipsum dolor sit amet consectetur. Gravida id amet id maecenas tellus."
-                    >
-                      <span style={{ float: "right" }} role="button">
-                        <img src={info} alt="more" />
-                      </span>
-                    </Tooltip>
-                  </p>
-                </div>
-              </Card>
-            </div>
-            <div className="col-9">
-              <ShipmentTracker Details={Details}/>
-              {/* <QuoteRequest /> */}
-            </div>
-          </div>
+        <div className="quotationresult-leftdiv" style={{ flex: "1 1 auto" }}>
+          <ShipmentTracker Details={Details} />
+          {/* <QuoteRequest /> */}
         </div>
       </div>
     </div>
