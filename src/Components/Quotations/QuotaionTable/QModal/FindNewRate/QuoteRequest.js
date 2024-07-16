4964c4d3eac8f61005b876439ebd7d6d74b92fdc
Sugitha08 <142788570+Sugitha08@users.noreply.github.com>
2024-07-16 10:05:22 +0530

    requestquote

--- src/Components/Quotations/QuotaionTable/QModal/FindNewRate/QuoteRequest.js
+++ src/Components/Quotations/QuotaionTable/QModal/FindNewRate/QuoteRequest.js
@@ -1,126 +1,3 @@
-// import React from "react";
-// import { Card, Col, Row, Input } from "antd";
-// import "./FindNewRate.css";
-// import Stripes from "../../../../../assets/Stripes.png";
-// import Avatar from "../../../../../assets/Avatar.png";
-// import RightArrow from "../../../../../assets/rightarrow.png";
-// import Email from "../../../../../assets/Email.png";
-// import info from "../../../../../assets/Info.svg";
-// import SubmitArr from "../../../../../assets/monochrome.png";
-
-// function QuoteRequest() {
-//   return (
-//     <>
-//       <Card className="Quote-Card">
-//         <Row>
-//           <Col span={24}>
-//             <img src={Stripes} alt="lines" className="img-stripe" />
-//             <img src={Avatar} alt="Avatar" className="img-avatar" />
-//           </Col>
-//           <Col span={24}>
-//             <div className="content">
-//               <p className="content-title">Non-Instant Quote request</p>
-//               <p className="content-description">
-//                 Sorry for the inconvenience . Currently We couldn't find any
-//                 instant results for this Non-Stackable Cargo. You can continue
-//                 to book and our operations team will get in touch with you
-//                 additional charges
-//               </p>
-//             </div>
-//           </Col>
-//           <Col span={24} className="mt-4">
-//             {/* <div className="request-btn">Request Non-Instant Quote</div> */}
-//             <div className="form">
-//               <p
-//                 style={{
-//                   margin: "0",
-//                   fontSize: "14px",
-//                   fontWeight: "500",
-//                   lineHeight: "24px",
-//                   letterSpacing: "1%",
-//                   color: "#67788E",
-//                 }}
-//               >
-//                 Email
-//               </p>
-//               <Input
-//                 size="large"
-//                 placeholder="johnedoe@gmail.com"
-//                 className="mb-3"
-//                 prefix={<img src={Email} alt="email" />}
-//                 suffix={
-//                   <p
-//                     style={{
-//                       fontWeight: "400",
-//                       fontSize: "13px",
-//                       lineHeight: "19px",
-//                       letterSpacing: "1%",
-//                       margin: "0px",
-//                       color: "#2C83EC",
-//                       cursor: "pointer",
-//                     }}
-//                   >
-//                     Verify
-//                   </p>
-//                 }
-//               />
-//               <p
-//                 style={{
-//                   margin: "0",
-//                   fontSize: "13px",
-//                   fontWeight: "500",
-//                   lineHeight: "19px",
-//                   letterSpacing: "1%",
-//                   color: "#181E25",
-//                 }}
-//               >
-//                 MSMD Document
-//                 <img src={info} alt="more" className="ms-1" />
-//               </p>
-//               <Input
-//                 size="large"
-//                 placeholder="You can drag & drop here"
-//                 className="mt-1"
-//                 addonAfter="Upload"
-//               />
-//               <div className="submit-btn">
-//                 Submit
-//                 <img src={SubmitArr} alt="arr" className="ms-2" />
-//               </div>
-//             </div>
-//             <div className="go-back">
-//               <div>
-//                 <img
-//                   src={RightArrow}
-//                   alt="arrow"
-//                   className="me-1"
-//                   style={{ width: "18px", height: "18px", cursor: "pointer" }}
-//                 />
-//               </div>
-//               <div>
-//                 <span
-//                   style={{
-//                     fontSize: "14px",
-//                     fontWeight: "400",
-//                     lineHeight: "24px",
-//                     letterSpacing: "1%",
-//                     color: "#000000",
-//                     cursor: "pointer",
-//                   }}
-//                 >
-//                   Go Back
-//                 </span>
-//               </div>
-//             </div>
-//           </Col>
-//         </Row>
-//       </Card>
-//     </>
-//   );
-// }
-
-// export default QuoteRequest;
-
 import React, { useState } from "react";
 import { Card, Col, Row, Button, Image } from "antd";
 import "./FindNewRate.css";
@@ -186,35 +63,9 @@ function QuoteRequest({ setShowReselt, checkedItems }) {
     } else if (checkedItems?.NonHarzardousCargo === false) {
       return <>Add Harzardous Cargo charges</>;
     } else {
-      return (
-        <>
-          <span
-            style={{
-              width: "18px",
-              height: "18px",
-            }}
-          ></span>
-          Go Back
-        </>
-      );
+      return <>Go Back</>;
     }
   };
-  const image =
-    checkedItems?.NonHarzardousCargo !== false &&
-    checkedItems?.StackableCargo !== false ? (
-      <Image
-        src={RightArrow}
-        alt="arrow"
-        className="me-3"
-        preview={false}
-        style={{
-          cursor: "pointer",
-          color: "#69b1ff",
-        }}
-      />
-    ) : (
-      ""
-    );
   return (
     <>
       <Card className="Quote-Card" style={{ padding: "0px" }}>
@@ -267,44 +118,27 @@ function QuoteRequest({ setShowReselt, checkedItems }) {
                 icon={
                   checkedItems?.NonHarzardousCargo !== false &&
                   checkedItems?.StackableCargo !== false ? (
-                    <LeftOutlined style={{ width: "13px", height: "13px",marginTop:"8px" }} />
+                    <LeftOutlined
+                      style={{
+                        width: "12px",
+                        height: "12px",
+                        marginTop: "10px",
+                      }}
+                    />
                   ) : (
                     ""
                   )
                 }
+                style={{
+                  fontSize: "14px",
+                  fontWeight: "400",
+                  lineHeight: "24px",
+                  letterSpacing: "1%",
+                  cursor: "pointer",
+                }}
               >
                 {ButtonLabel()}
               </Button>
-              {/* <div className="go-back">
-                  <div>
-                    <img
-                      src={RightArrow}
-                      alt="arrow"
-                      className="me-1"
-                      style={{
-                        width: "18px",
-                        height: "18px",
-                        cursor: "pointer",
-                        color: "#03b2cb",
-                      }}
-                    />
-                  </div>
-
-                  <div onClick={() => setShowReselt(false)}>
-                    <span
-                      style={{
-                        fontSize: "14px",
-                        fontWeight: "400",
-                        lineHeight: "24px",
-                        letterSpacing: "1%",
-                        color: "#03b2cb",
-                        cursor: "pointer",
-                      }}
-                    >
-                      Go Back
-                    </span>
-                  </div>
-                </div> */}
             </div>
           </Col>
         </Row>
